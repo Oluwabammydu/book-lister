31bec0a1a57df04a0c671a4980714a17fcf4db8f
[Oluwabammydu] <[oluwabamise@gmail.com]>
2020-04-08 03:01:20 +0100

    Adding the search feature

--- app.js
+++ app.js
@@ -8,36 +8,29 @@ bookList.addEventListener('click', function(e){
     }
 });
 
-
 //add book
 const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type=text').value;
-
 //create elements
 const li = document.createElement('li');
 const bookName = document.createElement('span');
 const deleteButton = document.createElement('span');
-
 //add content
 bookName.textContent = value;
 deleteButton.textContent = 'delete';
-
 //add classname
 bookName.classList.add('name');
 deleteButton.classList.add('delete');
-
 //append DOM
 li.appendChild(bookName);
 li.appendChild(deleteButton);
 bookList.appendChild(li)
 })
 
-
 //hide books
 const hideBook = document.querySelector('#hide');
-
 hideBook.addEventListener('change', function(e){
     if(hideBook.checked){
         bookList.style.display = 'none';
@@ -45,3 +38,23 @@ hideBook.addEventListener('change', function(e){
         bookList.style.display = 'block';
     }
 })
+
+//filter
+const searchBooks = document.forms['search-books'];
+searchBooks.addEventListener('keyup', function(e){
+    const searchValue = searchBooks.querySelector('input').value.toLowerCase();
+    const books = bookList.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const bookTitle = book.firstElementChild.textContent;
+        if(bookTitle.toLowerCase().indexOf(searchValue) != -1){
+            book.style.display = 'block';
+        } else{
+            book.style.display = 'none';
+        }
+    })
+    
+
+})
+
+
+
